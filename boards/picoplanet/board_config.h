40490a8392cfcad5bfc7c5845aba58ce74e16f94
Sven M. Hallberg <pesco@khjk.org>
2023-07-26 21:11:46 +0200

    add picoplanet

---
+++ boards/picoplanet/board_config.h
@@ -0,0 +1,17 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+
+#define VENDOR_NAME "bleeptrack"
+#define PRODUCT_NAME "PicoPlanet"
+#define VOLUME_LABEL "PLANETBOOT"
+#define INDEX_URL "https://picoplanet.bleeptrack.de/"
+#define BOARD_ID "SAMD21E18A-PicoPlanet-v0"
+
+#define USB_VID 0x239A
+#define USB_PID 0x001E
+
+#define LED_PIN PIN_PA05
+
+#endif
